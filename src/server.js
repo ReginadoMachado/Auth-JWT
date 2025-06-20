4fa455495c547ff5251a192f37dde4aebd2fee21
Reginaldo Machado <111655927+ReginadoMachado@users.noreply.github.com>
2025-06-20 00:37:50 -0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -2,9 +2,9 @@ require('dotenv').config();
 const express = require('express');
 const routes = require('./routes/authRoutes');
 const app = express();
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(routes);
 
-const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
